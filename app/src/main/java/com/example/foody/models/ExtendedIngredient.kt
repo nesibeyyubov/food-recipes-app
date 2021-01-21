b6a1c31609183efef69643bc802517c219aa2530
nesibeyyubov <nesibeyyubov2000@gmail.com>
2021-01-21 16:16:59 +0400

    first

---
+++ app/src/main/java/com/example/foody/models/ExtendedIngredient.kt
@@ -0,0 +1,20 @@
+package com.example.foody.models
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+
+data class ExtendedIngredient(
+    @SerializedName("amount")
+    val amount: Double?,
+    @SerializedName("consistency")
+    val consistency: String?,
+    @SerializedName("image")
+    val image: String = "",
+    @SerializedName("name")
+    val name: String = "",
+    @SerializedName("original")
+    val original: String?,
+    @SerializedName("unit")
+    val unit: String?
+): Serializable
